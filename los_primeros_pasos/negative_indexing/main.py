6a0b2753d38c0565172606a1b34a4f5e8b7e136b
marcoinxs <>
2026-04-09 16:44:02 +0000

    Negative indexing

--- los_primeros_pasos/negative_indexing/main.py
+++ los_primeros_pasos/negative_indexing/main.py
@@ -1,17 +1,19 @@
 import pandas as pd
 
 cars_data = {'model': ['Audi A1', 'Audi A6', 'Audi A4', 'Audi A3','Audi A1'],
-          'year': [2017, 2016, 2017, 2019, 2016],
-          'fueltype': ['petrol', 'diesel', 'diesel', 'petrol', 'petrol'],
-          'price': [12500, 16500, 16800, 17300, 13900]}
+             'year': [2017, 2016, 2017, 2019, 2016],
+             'fueltype': ['petrol', 'diesel', 'diesel', 'petrol', 'petrol'],
+             'price': [12500, 16500, 16800, 17300, 13900]}
 
 audi_cars = pd.DataFrame(cars_data)
 
-# Write your code below
-audi_A1_2017 = audi_cars[(audi_cars['model'] == 'Audi A1') & (audi_cars['year'] == 2017)]
-audi_A1_2016 = audi_cars[(audi_cars['model'] == 'Audi A1') & (audi_cars['year'] == 2016)]
-audi_A3 = audi_cars[audi_cars['model'] == 'Audi A3']
-                    
+# 1. Audi A1 2017 está en la posición 0
+audi_A1_2017 = audi_cars.iloc[0]
+# 2. Audi A1 2016 está en la posición 4
+audi_A1_2016 = audi_cars.iloc[4]
+# 3. Audi A3 está en la posición 3
+audi_A3 = audi_cars.iloc[3]
+
 # Testing the result
 print(audi_A1_2017)
 print(audi_A1_2016)
